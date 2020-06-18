01ca5b9b91f808b9c4656c58d639c84fdce9ce1a
Maureen Sitienei <MSITIENEI@safaricom.co.ke>
2020-06-18 23:41:29 +0300

    add jquery hover effect in all elements in portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,31 @@ $(document).ready(function(){
   //add effect in design section
   $(".design").hover(function(){
     //trigger when mouse hover
-    $(".design-img").fadeTo(1000, 0.2);
+    $(".design-img").fadeTo(500, 0.2);
     $(".design-caption").show();
   }, function(){
     //trigger when mouse out
     $(".design-caption").hide();
-    $(".design-img").fadeTo(1000, 1);
+    $(".design-img").fadeTo(500, 1);
   });
   //add effect in development section
   $(".dev").hover(function(){
     //trigger when mouse hover
-    $(".dev-img").fadeTo(1000, 0.2);
+    $(".dev-img").fadeTo(500, 0.2);
     $(".dev-caption").show();
   }, function(){
     //trigger when mouse out
     $(".dev-caption").hide();
-    $(".dev-img").fadeTo(1000, 1);
+    $(".dev-img").fadeTo(500, 1);
+  });
+  //add effect in development section
+  $(".product").hover(function(){
+    //trigger when mouse hover
+    $(".product-img").fadeTo(500, 0.2);
+    $(".product-caption").show();
+  }, function(){
+    //trigger when mouse out
+    $(".product-caption").hide();
+    $(".product-img").fadeTo(500, 1);
   });
 });
